9f2e8f4240f1641fed7bfa705b0b9d582dc54edd
Abdul-wahid5544 <156010095+Abdul-wahid5544@users.noreply.github.com>
2024-02-29 00:26:27 +0500

    Update and rename index.js to maths-and-number.js

---
+++ maths-and-number.js
@@ -0,0 +1,33 @@
+// console.log(Maths.random());
+  // console.log(Math.random());
+  // console.log((Math.random()*10)+1)
+  // console.log(Math.floor(((Math.random()*10)+1)))
+
+  // the value must in between 20 to 50
+
+  // min = 20
+  // max = 50
+
+  // console.log(Math.floor(((Math.random()*(max-min+1)+min))))
+
+
+
+  // i will make the dice 6
+
+  // min = 1
+  // max = 6
+
+  
+  // console.log(Math.floor(((Math.random()*(max-min+1)+min))))
+
+
+
+
+  // ****************************************************number
+  console.log(Number)
+
+  let value = 101
+  console.log(value)
+  let balance = new Number(100)
+  console.log(balance)
+  console.log(value.length)
